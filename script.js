9b9af53fddf334e0b8b44051ff62e4d7580a6d97
annamani <annamanik.vit@gmail.com>
2026-02-02 10:55:33 +0100

    adding files to my new branch

---
+++ script.js
@@ -0,0 +1,5 @@
+// const cardFront = document.querySelectorAll(".card-front");
+// const cardBack = document.querySelectorAll(".card-back");
+// cardFront.addEventListener("click", () => {
+//     cardFront
+// })
