1ab6f975a573153d4aad55ee246cc0c21c8c2fcd
annamani <annamanik.vit@gmail.com>
2026-02-02 11:25:57 +0100

    created timer functionality

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-// const cardFront = document.querySelectorAll(".card-front");
-// const cardBack = document.querySelectorAll(".card-back");
-// cardFront.addEventListener("click", () => {
-//     cardFront
-// })
+const startButton = document.querySelectorAll('.start-button');
+const resetButton = document.querySelectorAll('.reset-button');
+const moveCounters = document.querySelectorAll('.move-counter');
+const timerElement = document.querySelectorAll('.timer');
+let moveCount = 0;
+let timerInterval;
+
+function startTimer(duration) {
+    let timeLeft = duration;
+    timerInterval = setInterval(() => {
+        timeLeft--;
+        if (timeLeft === 15) {
+            timerElement[0].style.color = "red";
+            alert("Hurry up! Only 15 seconds left.");
+        }
+        else if (timeLeft >= 0) {
+            timerElement[0].textContent = timeLeft;
+        }
+        else {
+            showResult();
+        }
+    }, 1000);
+}
+startTimer(120);
+
+
